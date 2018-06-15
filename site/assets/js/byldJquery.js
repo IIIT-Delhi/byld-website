2a6b4a1121dea50bc6257b22437c650e9500fe53
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-06-15 18:00:39 +0530

    Port homepage succesfully
    Several bufixed from the existing code
    most notably fix the navigation bar and get both scrollspy
    and smoothscroll working

---
+++ site/assets/js/byldJquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$("#wutIsByld").waypoint(function(){
+		$('#wutIsByld').addClass('animated fadeIn');
+	});
+	$("#htjCont").waypoint(function(){
+		$('#htj').addClass('animated fadeIn');
+	});
+	$("#compCont").waypoint(function(){
+		$('#comp').addClass('animated fadeIn');
+	});
+
+	$("#eventsCont").waypoint(function(){
+		$('#eventsCont li').addClass('animated fadeIn');
+	});
+	$("#projCont").waypoint(function(){
+		$('#projCont ul').addClass('animated fadeIn');
+	});
+	$("#eventDesc").waypoint(function(){
+		$('#eventDesc').addClass('animated fadeIn');
+	});
+
+	
+	
+});
--- site/index.md
+++ site/index.md
@@ -3,4 +3,13 @@
 # To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
 
 layout: home
+title: IIIT-Delhi
+
+# Projects on the front page
+projects:
+  - project1:
+    projectName: CW Portal
+    contributors: Sambhav Satija
+    technologies: Django
+    status: Released
 ---
