3ef2e9bc8369f362d6f223b8b98f56387c2efd5a
Peeyush Kushwaha <peeyush.p97@gmail.com>
2018-06-16 22:11:43 +0530

    Replace smoothscroll with jquery.scrollTo plugin
    URL is also updated with hash fragment on clicking nav link

--- site/assets/js/byldJquery.js
+++ site/assets/js/byldJquery.js
@@ -15,5 +15,32 @@ $(document).ready(function(){
 	$("#projects").waypoint(function(){
 		$('#projects ul').addClass('animated fadeIn');
 	});
+
+  // Bind to the click of all links with a #hash in the href. Modified from https://gist.github.com/flesler/3f3e1166690108abf747
+  $('a[href^="#"]').click(function(e) {
+    // Prevent the jump and the #hash from appearing on the address bar
+    e.preventDefault();
+
+    var targetElement = $($(e.target).attr("href"));
+
+    var avgScrollSpeed = 1.5;
+
+    var distance = Math.abs(window.scrollY - targetElement.offset().top);
+
+    var duration = Math.min(distance/avgScrollSpeed, 1000);
+
+    // Scroll the window, stop any previous animation, stop on user manual scroll
+    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+    $(window).stop(true).scrollTo(this.hash, {
+      duration: duration, 
+      interrupt: true, 
+      onAfter: function () {
+
+      // Append #hash to address bar url after we're done scrolling to it
+      window.location.hash = $(e.target).attr("href").substring(1);
+    
+    }});
+
+  });
 	
 });
