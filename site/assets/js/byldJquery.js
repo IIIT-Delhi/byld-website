d679aa9ca8c7418b7037131e8ca296d4047266dc
Peeyush Kushwaha <peeyush.p97@gmail.com>
2018-06-16 21:30:27 +0530

    Rename IDs of main nav elements to be url-friendly

--- site/assets/js/byldJquery.js
+++ site/assets/js/byldJquery.js
@@ -1,24 +1,19 @@
 $(document).ready(function(){
-	$("#wutIsByld").waypoint(function(){
-		$('#wutIsByld').addClass('animated fadeIn');
+	$("#what-is-byld").waypoint(function(){
+		$('#what-is-byld').addClass('animated fadeIn');
 	});
 	$("#htjCont").waypoint(function(){
 		$('#htj').addClass('animated fadeIn');
 	});
-	$("#compCont").waypoint(function(){
+	$("#competitions").waypoint(function(){
 		$('#comp').addClass('animated fadeIn');
 	});
 
-	$("#eventsCont").waypoint(function(){
-		$('#eventsCont li').addClass('animated fadeIn');
+	$("#events").waypoint(function(){
+		$('#events li').addClass('animated fadeIn');
 	});
-	$("#projCont").waypoint(function(){
-		$('#projCont ul').addClass('animated fadeIn');
+	$("#projects").waypoint(function(){
+		$('#projects ul').addClass('animated fadeIn');
 	});
-	$("#eventDesc").waypoint(function(){
-		$('#eventDesc').addClass('animated fadeIn');
-	});
-
-	
 	
 });
